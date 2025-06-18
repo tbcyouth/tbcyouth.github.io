8aa94eec77874d327d48aa29683df7c92ab30c14
Yem Stan <tbc.church.youth@gmail.com>
2025-06-18 23:50:41 +0500

    Almost full schedule

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
                 <div className="flex items-center gap-y-2 justify-between flex-col md:flex-row">
                     <div className="">© «TASHKENT BETHEL CHURCH», {new Date().getFullYear()}</div>
                     <div className="">
-                        Сделал с любовью: <a href="https://vladislavvlasov.github.io/" target="_blank" rel="noreferrer" className="font-medium">Владислав</a>
+                        Сделал с любовью: <a href="https://vladislavvlasov.github.io/" target="_blank" rel="noreferrer" className="font-medium">Владислав</a>, (Помог: <a href="https://github.com/1owski11" target="_blank" rel="noreferrer" className="font-medium">YSV</a>)
                     </div>
                 </div>
             </div>
