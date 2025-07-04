a90ad2c59757bae2b1984bcae8d604a8e0b21902
tbcyouth <tbc.church.youth@gmail.com>
2025-07-04 11:24:33 +0500

    fixed pairs Vlad & Rivko

--- src/components/ui/PrayRound.jsx
+++ src/components/ui/PrayRound.jsx
@@ -28,13 +28,23 @@ const algorithms = {
         [ [0, 4], [2, 6], [3, 5], [1, 8], [7, 11], [9, 13], [10, 12] ],
         [ [0, 2], [1, 3], [4, 6], [5, 12], [7, 9], [8, 10], [11, 13] ],
     ],
+    114: [
+        [ [0, 6], [1, 5], [2, 4], [3, 10], [7, 13], [8, 12], [9, 11] ],
+        [ [0, 3], [1, 2], [5, 6], [4, 11], [7, 10], [8, 9], [12, 13] ],
+        [ [1, 6], [2, 5], [3, 4], [0, 7], [8, 13], [9, 12], [10, 11] ],
+        [ [1, 3], [4, 5], [6, 2], [0, 13], [7, 8], [9, 10], [11, 12] ],
+        [ [0, 1], [2, 3], [4, 6], [5, 9], [7, 12], [8, 11], [10, 13] ],
+        [ [0, 4], [2, 6], [3, 5], [1, 8], [7, 11], [9, 13], [10, 12] ],
+        [ [0, 2], [1, 3], [4, 6], [5, 12], [7, 9], [8, 10], [11, 13] ],
+    ],
 };
 
 export default function PrayRound({ allMembers = [], groupIds = [], roundId }) {
     const roundIndex = Number(roundId);
     const groupSize = allMembers.length;
-    const roundPairs = algorithms[groupSize]?.[roundIndex];
     const group = getAuthGroup();
+
+    let roundPairs = (groupIds.length === 2 && groupIds[0] === 1) ? algorithms[groupSize + 100]?.[roundIndex] : algorithms[groupSize]?.[roundIndex];
 
     if (!roundPairs) {
         return <div>Нет данных для такого раунда или размера группы. ({groupSize} участников, раунд {roundId})</div>;
